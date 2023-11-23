0d2fb5c4373130b2636a3f09988d2e3b356b65ff
Marcel R. <github.riga@icloud.com>
2023-11-23 13:21:11 +0100

    Version 0.2.1.

--- mermaidmro/__meta__.py
+++ mermaidmro/__meta__.py
@@ -11,4 +11,4 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/mermaidmro"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
