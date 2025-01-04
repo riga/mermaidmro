8d9ea3ea5a4230dfabbf4bf54940762a29b6e458
Marcel Rieger <riga@users.noreply.github.com>
2025-01-04 20:30:51 +0100

    License.

--- mermaidmro/__meta__.py
+++ mermaidmro/__meta__.py
@@ -6,7 +6,7 @@ Create mermaid graphs from the method resolution order (mro) of Python objects.
 
 __author__ = "Marcel Rieger"
 __email__ = "github.riga@icloud.com"
-__copyright__ = "Copyright 2023-2024, Marcel Rieger"
+__copyright__ = "Copyright 2023-2025, Marcel Rieger"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/mermaidmro"
 __license__ = "BSD-3-Clause"
